42971a79220dc962715f172eb0fb4a1bacf9a265
pedrosapiens <pedro@sapienslearning.com>
2021-11-06 10:18:51 +0100

    initial

---
+++ js/main.js
@@ -0,0 +1,13 @@
+// Asignación de tipo de dato Array (Arreglos)
+
+let frutas = ['pera','manzana','naranja','platano'];
+
+console.log(frutas);
+
+let frutaSeleccionada = frutas[1];
+
+console.log(frutaSeleccionada);
+
+frutas[2] = 'kiwi';
+
+console.log(frutas);
